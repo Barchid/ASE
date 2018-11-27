3c08434fdeb3491c02b0d535b12a6ad0a2c4acfa
Barchid <sami.barchid@outlook.com>
2018-11-27 23:01:39 +0100

    fix test.sh

--- TP_08/Test.sh
+++ TP_08/Test.sh
@@ -1,11 +1,12 @@
 rm vdiskA.bin #Retirer le disque  d'avant
 make clean #clean le projet
 make # compiler les programmes
-export CURRENT_VOL=1 # Le volume courant est 1
+clear # enlever les lignes
+export current_vol=1 # Le volume courant est 1
 export HW_CONFIG="hw.h" # Pour aller chercher le fichier hardware qui contient des constantes
 
 echo "Creer la partition 0 de 20 blocs en (0,1)\n"
-./mkvol 0 1 20 # creer la partition n°00 de 20 blocs en cylindre = 0 et sector = 1
+./mkvol 0 1 10 # creer la partition n°00 de 20 blocs en cylindre = 0 et sector = 1
 
 echo "\nCreer la partition 1 de 100 blocs en (1,0)\n"
 ./mkvol 1 0 100 # creer la partition n°01 de 100 blocs en cyl=1, sec=0
@@ -18,11 +19,3 @@ echo "\nCréer un file system sur partition 1"
 
 echo "\nEtat de la partition 1"
 ./dfs
-
-echo "Créer le fichier 0 (lorem ipsum classique)"
-./if_nfile < LoremIpsum_Normal.txt
-
-echo "Créer le fichier 1 (Lorem ipsum très long)"
-./if_nfile < LoremIpsum_Long.txt
-
-echo ""
