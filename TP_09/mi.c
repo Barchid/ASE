47826742e1785ed76abc01acf43c36a1f1f3508b
Barchid Sami <barchid@a11p2.fil.univ-lille1.fr>
2018-11-28 10:47:40 +0100

    init files

--- TP_09/mi.c
+++ TP_09/mi.c
@@ -15,7 +15,7 @@ static int
 sum(void *ptr) 
 {
     int i;
-    int sum = 0;
+    int sum = 0; 
     
     for(i = 0; i < PAGE_SIZE * N/2 ; i++)
         sum += ((char*)ptr)[i];
---
+++ TP_09/mi.h
@@ -0,0 +1,18 @@
+/* ------------------------------
+   $Id: h.ins,v 1.1 2003-02-21 14:34:13 marquet Exp $
+   ------------------------------------------------------------
+
+   memory isolation 
+   Philippe Marquet, Nov 2017
+   
+*/
+
+#ifndef _MI_H_
+#define _MI_H_
+
+#define SYSCALL_SWTCH_0 16
+#define SYSCALL_SWTCH_1 17
+
+void init(void);
+
+#endif
